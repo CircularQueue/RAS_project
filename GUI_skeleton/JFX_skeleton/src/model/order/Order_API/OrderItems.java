f47d916f15f1cc5fa5825558c8f7b49030077251
benjammin12 <benxerri@gmail.com>
2017-04-24 22:03:26 -0400

    Order So Far

---
+++ GUI_skeleton/JFX_skeleton/src/model/order/Order_API/OrderItems.java
@@ -0,0 +1,15 @@
+
+public class OrderItems {
+	private int OrderID;
+	private int OrderItemID;
+	private int seatNumber;
+	private double itemPrice;
+	private String itemDescription;
+	private String itemName;
+
+	public OrderItems(int OrderID, int OrderItemID, int seatNumber, double itemPrice,String itemDescription,String itemName){
+		
+	}
+	
+
+}
---
+++ GUI_skeleton/JFX_skeleton/src/model/order/Order_API/README.md
@@ -0,0 +1,2 @@
+#This is Order so far
+##For now ignore orderItems.java, i'm not sure if I will use that, or extend MenuItems.java
