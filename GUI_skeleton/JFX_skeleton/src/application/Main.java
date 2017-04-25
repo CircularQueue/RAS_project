53230a4a7c7818c9a85e643315ef91f9db9cf3d2
PhillipWitkin <pwitkin273@gmail.com>
2017-04-25 03:32:30 -0400

    JDBC seems to work on PC

--- GUI_skeleton/JFX_skeleton/src/application/Main.java
+++ GUI_skeleton/JFX_skeleton/src/application/Main.java
@@ -7,6 +7,12 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Statement;
 
 public class Main extends Application {
 	@Override
@@ -21,6 +27,82 @@ public class Main extends Application {
 	}
 	
 	public static void main(String[] args) {
-		launch(args);
+//		launch(args);
+		
+		
+		// jdbc config experiment
+		  try {
+	            // The newInstance() call is a work around for some
+	            // broken Java implementations
+
+	            Class.forName("com.mysql.jdbc.Driver").newInstance();
+	        } catch (Exception ex) {
+	            // handle the error
+	        }
+		Connection conn = null;
+	
+		try {
+		    conn =
+		       DriverManager.getConnection("jdbc:mysql://127.0.0.1/test?" +
+		                                   "user=root&password=");
+
+		    // Do something with the Connection
+		    
+		   
+		} catch (SQLException ex) {
+		    // handle any errors
+		    System.out.println("SQLException: " + ex.getMessage());
+		    System.out.println("SQLState: " + ex.getSQLState());
+		    System.out.println("VendorError: " + ex.getErrorCode());
+		}
+		
+		java.sql.Statement stmt = null;
+		ResultSet rs = null;
+
+		try {
+		    stmt = conn.createStatement();
+		    rs = stmt.executeQuery("SELECT * FROM stuff");
+
+		    // or alternatively, if you don't know ahead of time that
+		    // the query will be a SELECT...
+
+//		    if (stmt.execute("SELECT foo FROM bar")) {
+//		        rs = stmt.getResultSet();
+//		    }
+
+		    // Now do something with the ResultSet ....
+		    while (rs.next()){
+		    	System.out.println("Title: " + rs.getString("Stuff_Title"));
+		    }
+		    
+		}
+		catch (SQLException ex){
+		    // handle any errors
+		    System.out.println("SQLException: " + ex.getMessage());
+		    System.out.println("SQLState: " + ex.getSQLState());
+		    System.out.println("VendorError: " + ex.getErrorCode());
+		}
+		finally {
+		    // it is a good idea to release
+		    // resources in a finally{} block
+		    // in reverse-order of their creation
+		    // if they are no-longer needed
+
+		    if (rs != null) {
+		        try {
+		            rs.close();
+		        } catch (SQLException sqlEx) { } // ignore
+
+		        rs = null;
+		    }
+
+		    if (stmt != null) {
+		        try {
+		            stmt.close();
+		        } catch (SQLException sqlEx) { } // ignore
+
+		        stmt = null;
+		    }
+		}
 	}
 }
