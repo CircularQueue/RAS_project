a4d24927272db79d82c2d12ee005d3b1f553b2b3
Benjamin Goodwin <Benjamin Goodwin>
2017-05-04 19:34:59 -0400

    Almost done

--- Menu/src/application/ViewController.java
+++ Menu/src/application/ViewController.java
@@ -1,53 +1,35 @@
 package application;
 
-import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.HashMap;
-import java.util.Optional;
 import java.util.ResourceBundle;
 
+import Main.Menu;
 import Main.MenuItem;
 import Main.MenuJDBC;
-import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonBar.ButtonData;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableColumn.CellEditEvent;
-import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
-import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.TextFlow;
-import javafx.stage.Modality;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import javafx.util.Pair;
 
 //import 
 // Note this is an extension of the container class BorderPane. The fxml must also have that as its root.
@@ -64,39 +46,50 @@ public class ViewController extends BorderPane implements Initializable{
 	    final SimpleStringProperty item_id;
 	    final SimpleStringProperty price;
 	    final SimpleStringProperty description;
+	    private String tempName;
+	    private int tempId;
+	    private Double tempPrice;
+	    private String tempDes;
 	    public MenuItemData (MenuItem item){
 			this.name = new SimpleStringProperty(item.getName());
 			this.item_id = new SimpleStringProperty(Integer.toString(item.getID()));
-			System.out.println("Here constructor");
 			this.price = new SimpleStringProperty(Double.toString(item.getPrice()));
 			this.description = new SimpleStringProperty(item.getDescription());
 	    }
 			
 			public String getName(){
-				System.out.println("here name");
 				return this.name.get();
 			}    
 			public String getItem_id(){
-				System.out.println("here id");
 				return this.item_id.get();
 			}
 			public String getPrice(){
-				System.out.println("here price");
 				return this.price.get();
 			}
 			public String getDescription(){
-				System.out.println("here des");
 				return this.description.get();
 			}
 			
-			public void setName(String name){
-				this.name=item_id.set(name);
+			public void setName(String name1){
+				this.tempName = name1;
+			}
+			
+			public void setPrice(Double price1){
+				this.tempPrice = price1;
+			}
+			
+			public void setDescription(String description){
+				this.tempDes = description;
+			}
+			
+			public MenuItem setItem(){
+				return new MenuItem(Integer.parseInt(getItem_id()), getName(), Double.parseDouble(getPrice()), getDescription());
 			}
 	}
 	
 	public ViewController(Stage stage) throws ClassNotFoundException, SQLException{
         Scene scene = new Scene(new Group());
-        stage.setWidth(450);
+        stage.setWidth(950);
         stage.setHeight(550);
         initializeTable();
         ((Group) scene.getRoot()).getChildren().addAll(vbox);
@@ -107,23 +100,24 @@ public class ViewController extends BorderPane implements Initializable{
 	
 	public void initializeTable() throws ClassNotFoundException, SQLException{
 		db=new MenuJDBC();
+		Menu menu = new Menu();
 		MenuItemData iData;
 	    final Label label = new Label("Menu");
+        label.setFont(new Font("Arial", 20));
 		itemTable.setEditable(true);
 		TableColumn item_name = new TableColumn("Item ID");
 		TableColumn item_id = new TableColumn("Item Name");
 		TableColumn item_price = new TableColumn("Item Price");
 		TableColumn item_description = new TableColumn("Item Description");
 		itemTable.getColumns().addAll(item_id, item_name, item_price, item_description);
-	    item_name.setMinWidth(100);
+	    item_name.setMinWidth(200);
 	    item_id.setMinWidth(100);
 	    item_price.setMinWidth(100);
-	    item_description.setMinWidth(100);
+	    item_description.setMinWidth(500);
         item_name.setCellValueFactory(new PropertyValueFactory<MenuItemData, String>("name"));
         item_id.setCellValueFactory(new PropertyValueFactory<MenuItemData, String>("item_id"));
         item_price.setCellValueFactory(new PropertyValueFactory<MenuItemData, String>("price"));
         item_description.setCellValueFactory(new PropertyValueFactory<MenuItemData, String>("description"));
-        db = new MenuJDBC();
         HashMap<Integer, MenuItem> hm = new HashMap<>();
         hm = db.populateMenu();
         
@@ -135,10 +129,75 @@ public class ViewController extends BorderPane implements Initializable{
                 public void handle(CellEditEvent<MenuItemData, String> t) {
                     ((MenuItemData) t.getTableView().getItems().get(
                             t.getTablePosition().getRow())
-                            ).setName((t.getNewValue()));
+                            ).setName(t.getNewValue());
+                    try {
+						menu.updateItem(((MenuItemData) t.getTableView().getItems().get(
+						        t.getTablePosition().getRow())
+						        ).setItem(), Integer.parseInt(((MenuItemData) t.getTableView().getItems().get(
+						                t.getTablePosition().getRow())
+						                ).getItem_id()));
+					} catch (NumberFormatException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (ClassNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
                 }
             }
         );
+		
+		item_price.setCellFactory(TextFieldTableCell.forTableColumn());
+		
+		item_price.setOnEditCommit(
+	            new EventHandler<CellEditEvent<MenuItemData, String>>() {
+	                @Override
+	                public void handle(CellEditEvent<MenuItemData, String> t) {
+	                    ((MenuItemData) t.getTableView().getItems().get(
+	                            t.getTablePosition().getRow())
+	                            ).setPrice(Double.parseDouble(t.getNewValue()));
+	                    try {
+							menu.updateItem(((MenuItemData) t.getTableView().getItems().get(
+							        t.getTablePosition().getRow())
+							        ).setItem(), Integer.parseInt(((MenuItemData) t.getTableView().getItems().get(
+							                t.getTablePosition().getRow())
+							                ).getItem_id()));
+						} catch (NumberFormatException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (ClassNotFoundException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+	                }
+	            }
+	        );
+		
+		item_description.setCellFactory(TextFieldTableCell.forTableColumn());
+		
+		item_description.setOnEditCommit(
+	            new EventHandler<CellEditEvent<MenuItemData, String>>() {
+	                @Override
+	                public void handle(CellEditEvent<MenuItemData, String> t) {
+	                    ((MenuItemData) t.getTableView().getItems().get(
+	                            t.getTablePosition().getRow())
+	                            ).setDescription(t.getNewValue());
+	                    try {
+							menu.updateItem(((MenuItemData) t.getTableView().getItems().get(
+							        t.getTablePosition().getRow())
+							        ).setItem(), Integer.parseInt(((MenuItemData) t.getTableView().getItems().get(
+							                t.getTablePosition().getRow())
+							                ).getItem_id()));
+						} catch (NumberFormatException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (ClassNotFoundException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+	                }
+	            }
+	        );
         
         itemTable.setItems(data);
         for(Integer key : hm.keySet()){
@@ -157,13 +216,18 @@ public class ViewController extends BorderPane implements Initializable{
         final TextField addItemDescription = new TextField();
         addItemDescription.setMaxWidth(item_description.getPrefWidth());
         addItemDescription.setPromptText("Item Description");
- 
+        
         final Button addButton = new Button("Add");
         addButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e) {
                 try {
-					db.insertData(new MenuItem(Integer.parseInt(addItemID.getText()), addItemName.getText(), Double.parseDouble(addItemPrice.getText()), addItemDescription.getText()));
+                	MenuItem item = new MenuItem(Integer.parseInt(addItemID.getText()), addItemName.getText(), Double.parseDouble(addItemPrice.getText()), addItemDescription.getText());
+					db.insertData(item);
+					MenuItemData itemData = new MenuItemData(item);
+					data.add(itemData);
+					itemTable.refresh();
+			        
 				} catch (NumberFormatException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
@@ -180,7 +244,40 @@ public class ViewController extends BorderPane implements Initializable{
                 addItemDescription.clear();
             }
         });
+        
+        final TextField removeItem = new TextField();
+        removeItem.setMaxWidth(item_id.getPrefWidth());
+        removeItem.setPromptText("Item ID");
+        
+        final Button removeButton = new Button("Remove");
+        removeButton.setOnAction(new EventHandler<ActionEvent>() {
+        	@Override
+            public void handle(ActionEvent e) {
+                try {
+                	MenuItem item = db.deleteData(Integer.parseInt(removeItem.getText()));
+					MenuItemData itemData = new MenuItemData(item);
+					data.remove(itemData);
+					itemTable.refresh();
+				} catch (NumberFormatException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (ClassNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+                addItemID.clear();
+                addItemName.clear();
+                addItemPrice.clear();
+                addItemDescription.clear();
+            }
+        });
+        
         hb.getChildren().addAll(addItemID, addItemName, addItemPrice, addItemDescription, addButton);
+        hb.setSpacing(4);
+        hb.getChildren().addAll(removeItem, removeButton);
         hb.setSpacing(4);
  
         vbox = new VBox();
@@ -188,10 +285,6 @@ public class ViewController extends BorderPane implements Initializable{
         vbox.setPadding(new Insets(10, 0, 0, 10));
         vbox.getChildren().addAll(label, itemTable, hb);
         
-	}
-	
-	public void addItem(){
-		
 	}
 
 	@Override
