14484a0f9295e74517f045b353a05caa7de5fbd3
soccertina39 <soccertina39@aim.com>
2017-05-08 18:12:05 -0400

    Add files via upload

---
+++ GUI_skeleton/JFX_skeleton/ConnectionJDBC.java
@@ -0,0 +1,65 @@
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionJDBC {
+	
+	   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	   static final String DB_URL = "jdbc:mysql://localhost:3306/RAS";
+
+	   //  Database credentials
+	   static final String USER = "root";
+	   static final String PASS = "nPCr5Qc_*";
+	   
+	public ConnectionJDBC() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static Connection getDB() {
+		   
+		   Connection db_connection = null;
+		
+		   try{
+		      //STEP 2: Register JDBC driver
+		      Class.forName(JDBC_DRIVER);
+
+		   }
+		   catch(ClassNotFoundException se){ //Catches if you don't have your JDBC driver
+			      //Handle errors for JDBC
+			      se.printStackTrace();
+		   }
+		   try {
+			   db_connection = DriverManager.getConnection(DB_URL,USER,PASS); //get connection to Database
+			   return db_connection;
+		   }catch(SQLException e){
+			   System.err.println(e.getMessage());
+		   }
+		   
+		   return db_connection;
+	   }
+	
+	// connects without connecting to the specific database
+	public static Connection connectServer(){
+		   Connection db_connection = null;
+			
+		   try{
+		      //STEP 2: Register JDBC driver
+		      Class.forName(JDBC_DRIVER);
+
+		   }
+		   catch(ClassNotFoundException se){ //Catches if you don't have your JDBC driver
+			      //Handle errors for JDBC
+			      se.printStackTrace();
+		   }
+		   try {
+			   db_connection = DriverManager.getConnection("jdbc:mysql://localhost:3306",USER,PASS); //get connection to Database
+			   return db_connection;
+		   }catch(SQLException e){
+			   System.err.println(e.getMessage());
+		   }
+		   
+		   return db_connection;
+	}
+}
